9d067684355ced989f8dd6e752705de3e8eb0962
Jay9875 <112554527+Jay9875@users.noreply.github.com>
2022-09-29 16:12:20 +0930

    sample data
    Add sample data in the database

---
+++ SampleData.sql
@@ -0,0 +1,11 @@
+INSERT INTO Question ( Question, Answer, Mark)
+VALUES ('Name?', 'abc', 3);
+
+INSERT INTO Students ( email, password, firstName, lastName)
+VALUES ( 'xxx@gmail.com', 'abc', 'J', 'G');
+
+INSERT INTO Teacher ( email, password, firstName, lastName)
+VALUES ('xxx@gmail.com', 'abc', 'J', 'G');
+
+INSERT INTO Answer (studentID, questionID, StudentAnswer)
+VALUES (1, 1, 'abc');
