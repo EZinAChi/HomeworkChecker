8e49585d0adb89467181b4ef8b55ed250eb2abd2
EZinAChi <90136383+EZinAChi@users.noreply.github.com>
2022-10-23 17:32:53 +1030

    add comments

--- Project source code in flask/flaskProject/app.py
+++ Project source code in flask/flaskProject/app.py
@@ -10,18 +10,28 @@ def index():
     return render_template('menu.html')
 
 
-def databasequery(query):
+# The function to link the database and to count the number of item outputted
+def countqueryitems(query):
     db = pymssql.connect(host="LocalHost", user="mysql", password='88888888', database="AdventureWorks2019")
     cursor = db.cursor()
     cursor.execute(query)
-    data = cursor.fetchone()
+    # data = cursor.fetchone()
+    data = cursor.fetchall()
+    q = 0
+    for queryitem in data:
+        q += 1
     db.close()
-    return data
+    # return data
+    return q
 
+
+# route to port /studentlogin for pulling studentlogin page to front end
 @app.route('/studentlogin')
 def studentlogin():
     return render_template('studentlogin.html')
 
+
+# route to port /student for transferring student_name from studentlogin page and pulling quiz page to front end
 @app.route('/student', methods=['GET', 'POST'])
 def student():
     if request.method == 'GET':
@@ -29,25 +39,27 @@ def student():
 
         return render_template('quiz.html', data=student_name)
 
-
+# route to port /query for transferring student_query from quiz page and calculate the mark
 @app.route('/query', methods=['GET', 'POST'])
 def studentqueryenter():
     if request.method == 'GET':
         student_query = request.args.get('student_query')
-        datafromdatabase1 = databasequery("SELECT * FROM Person.address")
-        datafromdatabase2 = databasequery(student_query)
+        datafromdatabase1 = countqueryitems(
+            "SELECT COUNT(*) AS transCount, SUM(Quantity) AS avgQuantity,  AVG(ActualCost) AS avgCost FROM Production.TransactionHistoryArchive")
+        datafromdatabase2 = countqueryitems(student_query)
+
         print(datafromdatabase1)
         print(datafromdatabase2)
         print(student_query)
 
         return render_template('result.html', data=compareresult(datafromdatabase1, datafromdatabase2))
 
+# calculate the mark
 def compareresult(query1, query2):
     if query1 == query2:
         return 1
     else:
         return 0
-
 
 
 if __name__ == '__main__':
