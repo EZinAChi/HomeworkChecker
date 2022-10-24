42d99c24ba53992b35784cbdfde4e2716120c321
EZinAChi <90136383+EZinAChi@users.noreply.github.com>
2022-10-24 22:19:36 +1030

    bug fixing

--- Project source code in flask/flaskProject/app.py
+++ Project source code in flask/flaskProject/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 
 import pymssql
-from pymssql import Answer, Deduction, Feedback, Practical, Question, Result, Students, Teacher
+
 
 app = Flask(__name__, template_folder='templates', static_folder='templates/layui')
 
@@ -16,7 +16,7 @@ def countqueryitems(query):
     db = pymssql.connect(host="LocalHost", user="mysql", password='88888888', database="AdventureWorks2019")
     cursor = db.cursor()
     cursor.execute(query)
-    # data = cursor.fetchone()
+    # if need the first item code: data = cursor.fetchone(), if need all item, code: data = cursor.fetchall()
     data = cursor.fetchall()
     q = 0
     for queryitem in data:
@@ -31,6 +31,7 @@ def countqueryitems(query):
 def studentlogin():
     return render_template('studentlogin.html')
 
+
 @app.route('/teacherlogin')
 def teacherlogin():
     return render_template('teacherlogin.html')
@@ -43,6 +44,7 @@ def student():
         student_name = request.args.get('student_name')
 
         return render_template('quiz.html', data=student_name)
+
 
 # route to port /student for transferring student_name from studentlogin page and pulling quiz page to front end
 @app.route('/teacher', methods=['GET', 'POST'])
@@ -62,12 +64,13 @@ def studentqueryenter():
             "SELECT COUNT(*) AS transCount, SUM(Quantity) AS avgQuantity,  AVG(ActualCost) AS avgCost FROM Production.TransactionHistoryArchive")
         datafromdatabase2 = countqueryitems(student_query)
 
-# print for testing
+        # print for testing
         print(datafromdatabase1)
         print(datafromdatabase2)
         print(student_query)
 
         return render_template('result.html', data=compareresult(datafromdatabase1, datafromdatabase2))
+
 
 # calculate the mark
 def compareresult(query1, query2):
@@ -76,47 +79,69 @@ def compareresult(query1, query2):
     else:
         return 0
 
+
 #  connect to main database
 def connectHWCdb():
     db = pymssql.connect(host="LocalHost", user="mysql", password='88888888', database="HomeworkChecker")
     return db
 
+
 #  insert data to Students table
 def insertstudent(studentid, email, password, firstName, lastName):
-
     db = connectHWCdb()
 
     label = ['studentID', 'email', 'password', 'firstName', 'lastName']
     content = [studentid, email, password, firstName, lastName]
 
-    sql = 'insert into {0} ({1},{2},{3},{4}) values({5},"{6}","{7}","{8}","{9}")'.format(Students,label[0],label[1],
-          label[2],label[3],label[4],content[0],content[1],content[2],content[3],content[4])
+    sql = 'insert into {0} ({1},{2},{3},{4}) values({5},"{6}","{7}","{8}","{9}")'.format("Students", label[0], label[1],
+                                                                                         label[2], label[3], label[4],
+                                                                                         content[0], content[1],
+                                                                                         content[2], content[3],
+                                                                                         content[4])
     result = db.execute(sql)
     db.commit()
-    return  True if result else False
+    return True if result else False
 
-#  insert data to Result table
-def insertresult(studentid, questionid, totalmark):
-
-    db = connectHWCdb()
-
-    label = ['studentID', 'questionID', 'totalMark']
-    content = [studentid, questionid, totalmark]
-
-    sql = 'insert into {0} ({1},{2}) values({3},"{4}","{5}")'.format(Result,label[0],label[1],
-          label[2],label[3],label[4],content[0],content[1],content[2],content[3],content[4])
-    result = db.execute(sql)
-    db.commit()
-    return  True if result else False
 
 #  read data from Students table
 def readstudent():
-
     db = connectHWCdb()
 
     sql = 'select * from {0}'.format(Students)
     result = db.execute(sql)
     return list(result)
+
+#  update student data
+def updatestudent(studentid, email, password, firstName, lastName):
+    db = connectHWCdb()
+
+    label = ['studentID', 'email', 'password', 'firstName', 'lastName']
+    content = [studentid, email, password, firstName, lastName]
+
+    sql = 'update Students set {0} ({1},{2},{3},{4}) values({5},"{6}","{7}","{8}","{9}" where studentID = {0})'.format("Students", label[0], label[1],
+                                                                                         label[2], label[3], label[4],
+                                                                                         content[0], content[1],
+                                                                                         content[2], content[3],
+                                                                                         content[4])
+    result = db.execute(sql)
+    db.commit()
+    return True if result else False
+
+
+#  insert data to Result table
+def insertresult(studentid, questionid, totalmark):
+    db = connectHWCdb()
+
+    label = ['studentID', 'questionID', 'totalMark']
+    content = [studentid, questionid, totalmark]
+
+    sql = 'insert into {0} ({1},{2}) values({3},"{4}","{5}")'.format("Result", label[0], label[1],
+                                                                     label[2], label[3], label[4], content[0],
+                                                                     content[1], content[2], content[3], content[4])
+    result = db.execute(sql)
+    db.commit()
+    return True if result else False
+
 
 #  run the main program
 if __name__ == '__main__':
