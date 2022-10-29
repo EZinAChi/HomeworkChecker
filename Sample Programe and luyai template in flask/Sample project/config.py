c0167e24c9e456b1f37f956e6348d93178c7756d
EZinAChi <90136383+EZinAChi@users.noreply.github.com>
2022-10-29 19:25:29 +1030

    sample code deleted

--- Sample Programe and luyai template in flask/Sample project/config.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-
-# mongo
-MONGO_CONF = 'mongodb://127.0.0.1:27017/?authSource=admin'
--- Sample Programe and luyai template in flask/Sample project/sites/person/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-"""
-File name    : __init__.py.py
-Author       : l-changxuanwei
-Create date  : 2022/9/13 8:08 下午 
-Description  : 
-"""
